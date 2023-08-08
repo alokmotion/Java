bbeaccc7b976564bd9a66f9a32bada9daf7dacd5
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2023-08-09 00:24:36 +0530

    Binary Operators

---
+++ LogicalOperator.java
@@ -0,0 +1,25 @@
+
+public class LogicalOperator {
+	public static void main(String[]args) {
+		
+		
+		System.out.println("and (&&) Operator");
+		System.out.println(true&&true);
+		System.out.println(true&&false);
+		System.out.println(false&&true);
+		System.out.println(false&&false);
+		
+		
+
+		System.out.println("or (||) Operator");
+		System.out.println(true||true);
+		System.out.println(true||false);
+		System.out.println(false||true);
+		System.out.println(false||false);
+		
+		
+		
+		
+		}
+
+}
