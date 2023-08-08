ad4eb63745b9c0d1e69ded9b89de7bdf1625d974
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2023-08-08 23:26:33 +0530

    Binary Operators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,12 @@
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		System.out.println(10+5);
+		System.out.println(10-5);
+		System.out.println(10*5);
+		System.out.println(10/5);
+		System.out.println(10%5);
+	}
+
+}
