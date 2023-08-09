d4d3cf2f3e9b664d2b24b5e8eab3b3adedf5b659
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2023-08-10 02:22:12 +0530

    Primitive Data Type

---
+++ IntegerDataType.java
@@ -0,0 +1,16 @@
+
+public class IntegerDataType {
+
+	public static void main(String[] args) {
+		byte age = 22;
+		System.out.println("Age is :" + age);
+		short year = 2023;
+		System.out.println("Year is : "+ year);
+		int salary = 40000;
+		System.out.println("Salary is : " + salary);
+		long population = 123456789123L;
+		System.out.println("Population is : " + population);
+		
+	}
+
+}
