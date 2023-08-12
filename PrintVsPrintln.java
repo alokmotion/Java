b60f0ead1faffa1ebcaa1ad002cbf4e0b525b683
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2023-08-13 02:26:21 +0530

    Print Vs Println Method

---
+++ PrintVsPrintln.java
@@ -0,0 +1,20 @@
+
+public class PrintVsPrintln {
+
+	public static void main(String[] args) {
+		System.out.println("System.out.println Method");
+		
+		System.out.println("Mango");
+		System.out.println("Apple");
+		System.out.println("Banana");
+		
+		System.out.println("");
+		System.out.println("System.out.print Method");
+		
+		System.out.print("Mango");
+		System.out.print("Apple");
+		System.out.print("Banana");
+
+	}
+
+}
