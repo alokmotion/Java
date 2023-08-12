100e05e51cac6a2495b00de6a016f7f0d8c1a42d
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2023-08-12 11:57:40 +0530

    Why Suffix L

---
+++ SuffixL.java
@@ -0,0 +1,13 @@
+
+public class SuffixL {
+	public static void main(String[]args) {
+		long a = 21474;
+		System.out.println(a);
+		
+		long b = 2147834567L;
+		System.out.println(b);
+		
+		
+	}
+
+}
